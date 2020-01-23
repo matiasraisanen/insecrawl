422bc4cca32f75d95eb61b2e592f62fe19d8403b
Matias Räisänen <matias.raisanen@gmail.com>
2020-01-23 14:07:14 +0200

    loadingBar now  starts at zero

--- insecrawl.py
+++ insecrawl.py
@@ -414,7 +414,7 @@ class Insecrawl:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
         req = Request(url=url, headers=headers)
-
+        self.LoadingBar(self.progressCounter, totalCams)
         try:
             html = urlopen(req).read()
             soup = BeautifulSoup(html, features="html.parser")
