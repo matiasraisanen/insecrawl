566a96fecb7157666b7c6b2d6a1d3c67e1810c43
Matias Räisänen <matias.raisanen@gmail.com>
2020-02-07 08:11:20 +0200

    add counter class

---
+++ Counter.py
@@ -0,0 +1,14 @@
+import multiprocessing
+
+
+class Counter(object):
+    def __init__(self):
+        self.val = multiprocessing.Value('i', 0)
+
+    def increment(self, n=1):
+        with self.val.get_lock():
+            self.val.value += n
+
+    @property
+    def value(self):
+        return self.val.value
