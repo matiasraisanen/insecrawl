390df4286fba6e0bea1ac77ab0edb13e1f62ecbf
Matias Räisänen <matias.raisanen@gmail.com>
2020-02-14 09:06:47 +0200

    add reset value

--- Counter.py
+++ Counter.py
@@ -9,6 +9,9 @@ class Counter(object):
         with self.val.get_lock():
             self.val.value += n
 
+    def reset(self):
+        self.val = multiprocessing.Value('i', 0)
+
     @property
     def value(self):
         return self.val.value
