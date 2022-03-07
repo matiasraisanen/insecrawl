bad6e59f9a03b037e5a2261e077fcd5c7139bb50
Matias Räisänen <matias.raisanen@gmail.com>
2022-03-07 08:47:24 +0200

    fix syntax

--- insecrawl.py
+++ insecrawl.py
@@ -539,11 +539,11 @@ class Insecrawl:
         d['hrs'], rem = divmod(tdelta.seconds, 3600)
         d['min'], d['sec'] = divmod(rem, 60)
 
-        if d['min'] is 0:
+        if d['min'] == 0:
             fmt = '{sec}s'
-        elif d['hrs'] is 0:
+        elif d['hrs'] == 0:
             fmt = '{min}m {sec}s'
-        elif d['days'] is 0:
+        elif d['days'] == 0:
             fmt = '{hrs}h {min}m {sec}s'
         else:
             fmt = '{days}d {hrs}h {min}m {sec}s'
